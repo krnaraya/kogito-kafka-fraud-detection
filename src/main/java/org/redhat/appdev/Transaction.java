577651aa0b65c0623f890d4ff51b95b28cafb381
Krishnan Narayana Swamy <krnaraya@redhat.com>
2020-08-08 16:17:36 +1000

    initial commit

---
+++ src/main/java/org/redhat/appdev/Transaction.java
@@ -0,0 +1,81 @@
+package org.redhat.appdev;
+
+public class Transaction {
+
+	private String name;
+	private String reference;
+	private String mode;
+	private String location;
+    private Integer amount;
+    private Boolean flagged;
+
+	public Transaction() {
+
+	}
+
+	public Transaction(final String name, final String reference, final String mode, final String location, final Integer amount, final Boolean flagged ) {
+		super();
+		this.name = name;
+		this.reference = reference;
+		this.mode = mode;
+		this.location = location;
+        this.amount = amount;
+        this.flagged = flagged;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(final String name) {
+		this.name = name;
+	}
+
+	public String getReference() {
+		return this.reference;
+	}
+
+	public void setReference(final String reference) {
+		this.reference = reference;
+	}
+
+	public String getMode() {
+		return this.mode;
+	}
+
+	public void setMode(final String mode) {
+		this.mode = mode;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(final String location) {
+		this.location = location;
+	}
+
+	public Integer getAmount() {
+		return this.amount;
+	}
+
+	public void setAmount(final Integer amount) {
+		this.amount = amount;
+    }
+    
+    public Boolean isFlagged() {
+		return this.flagged;
+	}
+
+	public void setFlagged(final Boolean flagged) {
+		this.flagged = flagged;
+	}
+
+	@Override
+	public String toString() {
+		return "Transaction [Name=" + name + ", reference=" + reference + ", mode=" + mode + ", location="
+				+ location + ", amount=" + amount + "]";
+	}
+
+}
+
