dbfb2bb6594d2a9210af0260b069b470ed14b0f4
Krishnan Narayana Swamy <krnaraya@redhat.com>
2020-08-19 15:55:36 +1000

    Added support for fraud and non fraud data

--- src/main/java/org/redhat/appdev/PredictFraud.java
+++ src/main/java/org/redhat/appdev/PredictFraud.java
@@ -1,16 +1,21 @@
 package org.redhat.appdev;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
 @RegisterRestClient
 public interface PredictFraud {
 
-    @GET
+    @POST
     @Path("/predict")
     @Produces("application/json")
-    PredictionResponse get();
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    PredictionResponse post(@FormParam("json") String data);
 }
