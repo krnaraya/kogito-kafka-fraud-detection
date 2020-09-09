43b345c1acd63f3111b557a2bf48272259eed982
Krishnan Narayana Swamy <krnaraya@redhat.com>
2020-09-09 10:12:43 +1000

    fixed

--- src/main/java/org/redhat/appdev/PredictFraud.java
+++ src/main/java/org/redhat/appdev/PredictFraud.java
@@ -2,7 +2,6 @@ package org.redhat.appdev;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.FormParam;
-import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
